96cf48f1ace7b9c2dbd6ce63bdf7b6225db8e2f3
locedu <locutus.edu@outlook.com>
2025-02-19 18:17:14 -0500

    authConfig (jwt expiration: 15min)

--- src/config/authConfig.js
+++ src/config/authConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: process.env.JWT_SECRET || "your-secure-secret",  // Change this to a secure value
-    jwtExpiresIn: "15m"  // Token expiration time (7 days)
+    jwtExpiresIn: "15m"  // Token expiration time..
 };
