c2b28bdb9f053def9e93af8fa77388f52de1bb3d
locedu <locutus.edu@outlook.com>
2025-03-12 19:19:45 -0400

    new route (PUT api/auth/me) update profile.

--- src/features/profile/controllers/profileController.js
+++ src/features/profile/controllers/profileController.js
@@ -1,8 +1,8 @@
 const prisma = require('../../../config/prismaClient');
 
+// Controller method to get the profile
 exports.getProfile = async (req, res) => {
     try {
-        // Get the user from the decoded JWT token attached to the request object
         const user = await prisma.user.findUnique({
             where: { id: req.user.id },
         });
@@ -14,5 +14,21 @@ exports.getProfile = async (req, res) => {
         res.status(200).json({ user });
     } catch (error) {
         res.status(500).json({ error: 'Server error' });
+    }
+};
+
+// Controller method to update the profile
+exports.updateProfile = async (req, res) => {
+    try {
+        const { name, statusMessage, bio } = req.body;
+
+        const updatedUser = await prisma.user.update({
+            where: { id: req.user.id },
+            data: { name, statusMessage, bio },
+        });
+
+        res.status(200).json({ message: "Profile updated successfully", updatedUser });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update profile' });
     }
 };
